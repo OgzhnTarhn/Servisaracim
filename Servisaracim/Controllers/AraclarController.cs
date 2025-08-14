ed7424e001c23c4af0f0cc6688871ad5fa5daaa2
Oğuzhan Tarhan <oguzhantarhan7467@gmail.com>
2025-08-14 14:46:04 +0300

    AraclarController are added new cshtml files are linked to my mainpage

---
+++ Servisaracim/Controllers/AraclarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Servisaracim.Controllers
+{
+    public class AraclarController : Controller
+    {
+        // GET: Araclar
+        public ActionResult MercedesSprinter()
+        {
+            return View();
+        }
+
+        public ActionResult OtokarSultan()
+        {
+            return View();
+        }
+        public ActionResult FiatDucato()
+        {
+            return View();
+        }
+    }
+}
--- Servisaracim/Global.asax.cs
+++ Servisaracim/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +17,27 @@ namespace Servisaracim
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            // Türkçe karakter desteği için encoding ayarları
+            System.Web.Mvc.ViewEngines.Engines.Clear();
+            System.Web.Mvc.ViewEngines.Engines.Add(new RazorViewEngine());
+        }
+        
+        protected void Application_BeginRequest()
+        {
+            // Her request'te UTF-8 encoding kullan
+            if (Response != null)
+            {
+                Response.ContentEncoding = Encoding.UTF8;
+                Response.HeaderEncoding = Encoding.UTF8;
+                Response.Charset = "utf-8";
+            }
+            
+            // Request encoding'i de UTF-8 yap
+            if (Request != null)
+            {
+                Request.ContentEncoding = Encoding.UTF8;
+            }
         }
     }
 }
